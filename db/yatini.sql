c2e3189b081c7c9be86ba8c3116a8b6fcb8780b6
Amaruvillca <129773390+Amaruvillca@users.noreply.github.com>
2024-05-26 20:51:49 -0400

    hhyu

--- db/yatini.sql
+++ db/yatini.sql
@@ -134,4 +134,35 @@ INSERT INTO juegos (
     NOW() -- Fecha de creación
 );
 select * from categoria;
-
+INSERT INTO califica (Puntuacion, id_juego, id_usuario)
+VALUES (4, 1, 1);
+INSERT INTO califica (Puntuacion, id_juego, id_usuario)
+VALUES (5, 1, 2);
+INSERT INTO califica (Puntuacion, id_juego, id_usuario)
+VALUES (1, 1, 3);
+select*from califica ;
+select*from juegos;
+ALTER TABLE juegos MODIFY imagen_juego LONGBLOB;
+INSERT INTO juegos (
+    nombre_juego,
+    imagen_juego,
+    Descripcion,
+    url_juego,
+    edad,
+    id_categoria,
+    id_usuario,
+    archivo_comprimido,
+    estado_revision,
+    fecha_creacion
+) VALUES (
+    'ratataaaaaaaaapu',
+    LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/h.jpg'), -- Ruta del archivo de imagen
+    'Un juego educativo para aprender matemáticas básicas.',
+    'https://www.ejemplo.com/juego-matematicas',
+    5, -- Edad recomendada
+    1, -- ID de la categoría correspondiente
+    1, -- ID del usuario que está añadiendo el juego
+    LOAD_FILE('C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CODIGO.rar'), -- Ruta del archivo comprimido
+    'En revisión',
+    NOW() -- Fecha de creación
+);
